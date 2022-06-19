0496c03608e92c36a38a192c7fe39d9222a86497
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-06-19 20:18:24 +0100

    Now switches between player and second player on click, board can also be reset but button needs to be linked up

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+const createPlayer = (name, symbol) => {
+    const assignPositionToBoard = (i) => {
+        let row1 = GameBoard.board[0]
+        let row2 = GameBoard.board[1]
+        let row3 = GameBoard.board[2]
+        let currentPosition = document.getElementById(i)
+
+        console.log(i)
+        switch(i) {
+            case i = 1: currentPosition.innerHTML = symbol
+            row1[0] = symbol
+            break;
+            case i = 2: currentPosition.innerHTML = symbol
+            row1[1] = symbol
+            break;
+            case i = 3: currentPosition.innerHTML = symbol
+            row1[2] = symbol
+            break;
+            case i = 4: currentPosition.innerHTML = symbol
+            row2[0] = symbol
+            break;
+            case i = 5: currentPosition.innerHTML = symbol
+            row2[1] = symbol
+            break;
+            case i = 6: currentPosition.innerHTML = symbol
+            row2[2] = symbol
+            break;
+            case i = 7: currentPosition.innerHTML = symbol
+            row3[0] = symbol
+            break;
+            case i = 8: currentPosition.innerHTML = symbol
+            row3[1] = symbol
+            break;
+            case i = 9: currentPosition.innerHTML = symbol
+            row3[2] = symbol
+            break;
+            default: console.log("Not applicable")
+            }
+    }
+    return {name, symbol, assignPositionToBoard}
+}
+const computer = createPlayer('Computer', 'o')
+const player = createPlayer('Josh', 'x')
+
+const Game = {
+    // logic here should have a function that take player/computer as a parameter based off of 
+    // who had a go last, and then for do parameter.assignPositionToBoard(position) which will
+    // need something to prevent overlap of smybols and there should also be something that
+    // calls the checkboard function within GameBoard each time
+    // checkboard function should end the game and pronounce a winner.
+    currentTurnTaker: player,
+    state: (currentPlayer) => {
+        if (currentPlayer === player) {
+            player.assignPositionToBoard(position)
+            Game.currentTurnTaker = computer
+        }
+        else if (currentPlayer === computer) {
+            computer.assignPositionToBoard(position)
+            Game.currentTurnTaker = player
+        }
+    }
+}
+
 const GameBoard = {
     board: 
       [[null, null, null],
@@ -30,12 +93,21 @@ const GameBoard = {
     if (board[1] === [symbol, symbol, symbol]){
       console.log("winner!")
     }
-  },
-    resetBoard: () => {GameBoard.board = [
+    else {
+        console.log("uh oh")
+    }
+    },
+    resetBoard: () => {
+        GameBoard.board = [
         [null, null, null],
         [null, null, null],
         [null, null, null]]
-    }
+
+        positionArr.forEach(num => {
+            document.getElementById(num).innerHTML = ''
+       });
+
+    },
     
   }
 
@@ -46,34 +118,11 @@ let position;
 positionArr.forEach(num => {
      document.getElementById(num).addEventListener('click', (e) => {
         position = parseInt(e.target.id)
-        assignPositionToBoard(position)
+        Game.state(Game.currentTurnTaker)
+
+        // player.assignPositionToBoard(position) use this later
     })
 });
 
-function assignPositionToBoard (i) {
-    let row1 = GameBoard.board[0]
 
-    console.log(i)
-    switch(i) {
-        case i = 1: console.log("deez nuts 1")
-        break;
-        case i = 2: console.log("deez nuts 2")
-        break;
-        case i = 3: console.log("deez nuts 3")
-        break;
-        case i = 4: console.log("deez nuts 4")
-        break;
-        case i = 5: console.log("deez nuts 5")
-        break;
-        case i = 6: console.log("deez nuts 6")
-        break;
-        case i = 7: console.log("deez nuts 7")
-        break;
-        case i = 8: console.log("deez nuts 8")
-        break;
-        case i = 9: console.log("deez nuts 9")
-        break;
-        default: console.log("not deez nuts default nuts")
-        }
-}
 
