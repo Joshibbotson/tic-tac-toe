5d2b559e184b1df6902aa301239320201d8391c6
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-06-19 20:55:33 +0100

    got winner checker to work, but needs a meaningful response over console.logging winner.

--- script.js
+++ script.js
@@ -4,46 +4,52 @@ const createPlayer = (name, symbol) => {
         let row2 = GameBoard.board[1]
         let row3 = GameBoard.board[2]
         let currentPosition = document.getElementById(i)
-
-        console.log(i)
+        // i'm like 90% sure I can make this work with a for each loop and remove a buck ton of code...//
+        if (currentPosition.innerHTML === `<p> ${player.symbol} </p>`|| currentPosition.innerHTML === `<p> ${player2.symbol} </p>`){
+            return
+        }
+        else {
         switch(i) {
-            case i = 1: currentPosition.innerHTML = symbol
+            case i = 1: currentPosition.innerHTML = `<p> ${symbol} </p>`
             row1[0] = symbol
             break;
-            case i = 2: currentPosition.innerHTML = symbol
+            case i = 2: currentPosition.innerHTML = `<p> ${symbol} </p>`
             row1[1] = symbol
             break;
-            case i = 3: currentPosition.innerHTML = symbol
+            case i = 3: currentPosition.innerHTML = `<p> ${symbol} </p>`
             row1[2] = symbol
             break;
-            case i = 4: currentPosition.innerHTML = symbol
+            case i = 4: currentPosition.innerHTML = `<p> ${symbol} </p>`
             row2[0] = symbol
             break;
-            case i = 5: currentPosition.innerHTML = symbol
+            case i = 5: currentPosition.innerHTML = `<p> ${symbol} </p>`
             row2[1] = symbol
             break;
-            case i = 6: currentPosition.innerHTML = symbol
+            case i = 6: currentPosition.innerHTML = `<p> ${symbol} </p>`
             row2[2] = symbol
             break;
-            case i = 7: currentPosition.innerHTML = symbol
+            case i = 7: currentPosition.innerHTML = `<p> ${symbol} </p>`
             row3[0] = symbol
             break;
-            case i = 8: currentPosition.innerHTML = symbol
+            case i = 8: currentPosition.innerHTML = `<p> ${symbol} </p>`
             row3[1] = symbol
             break;
-            case i = 9: currentPosition.innerHTML = symbol
+            case i = 9: currentPosition.innerHTML = `<p> ${symbol} </p>`
             row3[2] = symbol
             break;
             default: console.log("Not applicable")
             }
+        }
+        GameBoard.checkBoard(GameBoard.board, 'x')
+        GameBoard.checkBoard(GameBoard.board, 'o')
     }
     return {name, symbol, assignPositionToBoard}
 }
-const computer = createPlayer('Computer', 'o')
+const player2 = createPlayer('player2', 'o')
 const player = createPlayer('Josh', 'x')
 
 const Game = {
-    // logic here should have a function that take player/computer as a parameter based off of 
+    // logic here should have a function that take player/player2 as a parameter based off of 
     // who had a go last, and then for do parameter.assignPositionToBoard(position) which will
     // need something to prevent overlap of smybols and there should also be something that
     // calls the checkboard function within GameBoard each time
@@ -52,10 +58,10 @@ const Game = {
     state: (currentPlayer) => {
         if (currentPlayer === player) {
             player.assignPositionToBoard(position)
-            Game.currentTurnTaker = computer
+            Game.currentTurnTaker = player2
         }
-        else if (currentPlayer === computer) {
-            computer.assignPositionToBoard(position)
+        else if (currentPlayer === player2) {
+            player2.assignPositionToBoard(position)
             Game.currentTurnTaker = player
         }
     }
